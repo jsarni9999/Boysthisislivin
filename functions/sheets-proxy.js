d16df531ffdacfe104d836568de413c6dda9eb29
jsarni9999 <jsarni@verizon.net>
2026-05-30 13:20:40 -0400

    Update sheets-proxy.js

--- functions/sheets-proxy.js
+++ functions/sheets-proxy.js
@@ -1,1 +1,12 @@
-
+export async function onRequest() {
+  const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQB5DaeXCB71JJEirxddx3C5mJf8dGLMNoqVchwgwWb35gLY5BDwNiStZpUEW3-aU3PSwtAKSNyQjqV/pub?gid=485151315&single=true&output=csv';
+  const res = await fetch(SHEET_URL);
+  const text = await res.text();
+  return new Response(text, {
+    headers: {
+      'Content-Type': 'text/csv',
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'no-store'
+    }
+  });
+}
